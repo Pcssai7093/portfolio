5b2cdd2fa3698a8776057eceb9a6c2c13c3f35fd
raja <pcssai7093@gmail.com>
2023-07-16 10:29:34 +0530

    added data.json

--- src/Components/Leftbar.js
+++ src/Components/Leftbar.js
@@ -1,116 +1,128 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import styles from "./leftbar.module.css";
 import Profile from "./Profile";
 import Skills from "./Skills";
 import { motion } from "framer-motion";
+import axios from "axios";
 function Leftbar() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/Pcssai7093/portfolio/master/data.json"
+      )
+      .then((result) => {
+        console.log(result.data);
+        setData((p) => result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div>
       {/* {window.innerWidth < 1000 && "it less than 1000"} */}
-      <div className={styles.box}>
-        {/* <Profile /> */}
-        <motion.div
-          initial={{
-            x: window.innerWidth > 1000 && "35vw",
-            y: window.innerWidth > 1000 && "20vh",
-            scale: window.innerWidth > 1000 && 1.3,
-          }}
-          animate={{ x: 0, y: 0, scale: 1 }}
-          transition={{ duration: 3, delay: 3 }}
-          className={styles.box1}
-        >
-          <div>
-            <img
-              src="https://plus.unsplash.com/premium_photo-1671997600444-bded97fed063?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80"
-              alt=""
-              srcset=""
-              className={styles.dp}
-            />
-          </div>
-          <div className={`${styles.name} lbName`}>
-            POTTI{" "}
-            <motion.span
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 0.5 }}
-            >
-              CHANDRA
-            </motion.span>{" "}
-            <motion.span
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 1 }}
-            >
-              SEKHAR
-            </motion.span>{" "}
-            <motion.span
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 1.5 }}
-            >
-              SAI
-            </motion.span>{" "}
-          </div>
+      {data && (
+        <div className={styles.box}>
+          {/* <Profile /> */}
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 2 }}
-            className="links"
+            initial={{
+              x: window.innerWidth > 1000 && "30vw",
+              y: window.innerWidth > 1000 && "20vh",
+              scale: window.innerWidth > 1000 && 1.3,
+            }}
+            animate={{ x: 0, y: 0, scale: 1 }}
+            transition={{ duration: 3, delay: 3 }}
+            className={styles.box1}
           >
-            <motion.div className={styles.link}>
-              <i className="fa-brands fa-linkedin"></i>
-              <a href="https://www.linkedin.com/in/potti-chandra-sekhar-sai-716987221/">
-                Chandra Sekhar
-              </a>
-            </motion.div>
+            <div className={styles.imageOverlay}>
+              <img
+                src={data.profile_pic}
+                alt=""
+                srcset=""
+                className={styles.dp}
+              />
+            </div>
+            <div className={`${styles.name} lbName`}>
+              POTTI{" "}
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+              >
+                CHANDRA
+              </motion.span>{" "}
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 1 }}
+              >
+                SEKHAR
+              </motion.span>{" "}
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 1.5 }}
+              >
+                SAI
+              </motion.span>{" "}
+            </div>
             <motion.div
-              className={styles.link}
-
-              // className={styles.link}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 2 }}
+              className="links"
             >
-              <i className="fa-brands fa-github"></i>
-              <a href="https://github.com/Pcssai7093/">Pcssai7093</a>
-            </motion.div>
-            <motion.div className={styles.link}>
-              <i className="fa-regular fa-envelope"></i>
-              <a href="mailto:pcssai7093">pcssai7093@gmail.com</a>
-            </motion.div>
-            <motion.div className={styles.link}>
-              <i className=" fas fa-phone "></i>
-              <a href="tel:+917093692948">+917093692948</a>
+              <motion.div className={styles.link}>
+                <i className="fa-brands fa-linkedin"></i>
+                <a href="https://www.linkedin.com/in/potti-chandra-sekhar-sai-716987221/">
+                  Chandra Sekhar
+                </a>
+              </motion.div>
+              <motion.div
+                className={styles.link}
+
+                // className={styles.link}
+              >
+                <i className="fa-brands fa-github"></i>
+                <a href="https://github.com/Pcssai7093/">Pcssai7093</a>
+              </motion.div>
+              <motion.div className={styles.link}>
+                <i className="fa-regular fa-envelope"></i>
+                <a href="mailto:pcssai7093">pcssai7093@gmail.com</a>
+              </motion.div>
+              <motion.div className={styles.link}>
+                <i className=" fas fa-phone "></i>
+                <a href="tel:+917093692948">+917093692948</a>
+              </motion.div>
             </motion.div>
           </motion.div>
-        </motion.div>
-        {/* <Skills /> */}
-        <motion.div
-          className={styles.box2}
-          initial={{ opacity: window.innerWidth > 1000 && 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 3, delay: 4 }}
-        >
-          <div className={styles.heading}>Skills</div>
-          <SkillsSection
-            name="Programming"
-            skills={["C", "Java", "Js", "Haskell", "SQL", "Python"]}
-          />
-          <SkillsSection
-            name="Web Development"
-            skills={[
-              "React",
-              "Nodejs",
-              "Express",
-              "MongoDB",
-              "SQL",
-              "Azure Web Services",
-              "CI/CD",
-            ]}
-          />
-          <SkillsSection
-            name="Data"
-            skills={["Python", "SQL", "Numpy", "Pandas", "Data Processing"]}
-          />
-        </motion.div>
-      </div>
+          {/* <Skills /> */}
+          <motion.div
+            className={styles.box2}
+            initial={{ opacity: window.innerWidth > 1000 && 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3, delay: 4 }}
+          >
+            <div className={styles.heading}>Skills</div>
+            <SkillsSection
+              name="Programming"
+              skills={data.skills.PROGRAMMING}
+            />
+            <SkillsSection
+              name="Libraries/FrameWorks"
+              skills={data.skills.LIBRARIES_FRAMEWORKS}
+            />
+            <SkillsSection name="Databases" skills={data.skills.DATABASES} />
+            <SkillsSection
+              name="Tools/Platforms"
+              skills={data.skills.TOOLS_PLATFORMS}
+            />
+            <SkillsSection name="Other" skills={data.skills.OTHER} />
+          </motion.div>
+        </div>
+      )}
     </div>
   );
 }
